b82bccd86e5d257dcfb451fece69db6c432a568a
Kabir Asani <iamkabirasani@gmail.com>
2025-04-16 16:28:09 +0530

    Refactor HeroText component to include routing for buttons

--- app/blocks/hero-block/components/hero-text/HeroText.tsx
+++ app/blocks/hero-block/components/hero-text/HeroText.tsx
@@ -1,6 +1,10 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const HeroText = () => {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col justify-start">
       <h1 className="text-6xl font-black">
@@ -12,8 +16,22 @@ const HeroText = () => {
         Fast, lightweight, built on Web Standards. Support for any JavaScript runtime.
       </p>
       <div className="flex flex-row justify-start mt-8 gap-4">
-        <button className="btn btn-lg rounded-full bg-orange-500 hover:bg-orange-700">Get Started</button>
-        <button className="btn btn-soft btn-lg rounded-full">View on GitHub</button>
+        <button
+          className="btn btn-lg rounded-full bg-orange-500 hover:bg-orange-700"
+          onClick={() => {
+            router.push("/docs");
+          }}
+        >
+          Get Started
+        </button>
+        <button
+          className="btn btn-soft btn-lg rounded-full"
+          onClick={() => {
+            router.push("https://github.com/honojs/hono");
+          }}
+        >
+          View on GitHub
+        </button>
       </div>
     </div>
   );
